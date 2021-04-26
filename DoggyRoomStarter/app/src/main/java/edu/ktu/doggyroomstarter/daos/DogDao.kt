687f9f5252955d697f078050fb114fc105dbebe6
Jokubas Akramas <j.akramas@gmail.com>
2021-04-26 22:50:42 +0300

    Flow instead of fetching data each time

--- DoggyRoomStarter/app/src/main/java/edu/ktu/doggyroomstarter/daos/DogDao.kt
+++ DoggyRoomStarter/app/src/main/java/edu/ktu/doggyroomstarter/daos/DogDao.kt
@@ -5,11 +5,12 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import edu.ktu.doggyroomstarter.models.Dog
+import java.util.concurrent.Flow
 
 @Dao
 interface DogDao {
     @Query("SELECT * FROM dogs")
-    suspend fun getAllDogs(): List<Dog>
+    fun getAllDogs(): kotlinx.coroutines.flow.Flow<List<Dog>>
 
     @Insert
     suspend fun insertAll(vararg dog: Dog)
--- DoggyRoomStarter/app/src/main/java/edu/ktu/doggyroomstarter/viewmodels/DogViewModel.kt
+++ DoggyRoomStarter/app/src/main/java/edu/ktu/doggyroomstarter/viewmodels/DogViewModel.kt
@@ -1,40 +1,25 @@
 package edu.ktu.doggyroomstarter.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import edu.ktu.doggyroomstarter.database.DogDatabase
 import edu.ktu.doggyroomstarter.models.Dog
 import edu.ktu.doggyroomstarter.utils.MockData
 import kotlinx.coroutines.launch
 
 class DogViewModel(private val db: DogDatabase) : ViewModel() {
-    private val _dogs = MutableLiveData<List<Dog>>()
+    private val _dogs = db.dogDao().getAllDogs().asLiveData()
     val dogs: LiveData<List<Dog>>
         get() = _dogs
-
-    init {
-        getDogs()
-    }
 
     fun addDog() {
         viewModelScope.launch {
             db.dogDao().insertAll(MockData.getRandomDog())
-            getDogs()
-        }
-    }
-
-    private fun getDogs() {
-        viewModelScope.launch {
-            _dogs.postValue(db.dogDao().getAllDogs())
         }
     }
 
     fun deleteDog(dog: Dog) {
         viewModelScope.launch {
             db.dogDao().delete(dog)
-            getDogs()
         }
     }
 
